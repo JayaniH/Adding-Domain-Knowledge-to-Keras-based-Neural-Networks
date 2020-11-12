7c7b74d0db078156bedeb0bb16ab807e01231cdc
JayaniH <jayani.hewa77@gmail.com>
2020-11-12 08:51:03 +0530

    plot actual data/domain curve

--- domain_model.py
+++ domain_model.py
@@ -1,9 +1,22 @@
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt 
 import datasets
 
 parameters = {}
+dominant_apis = ['ballerina/http/Caller#respond',
+'ballerina/http/Client#get#http://postman-echo.com',        #reg
+'ballerina/http/Client#get#https://covidapi.info/api/v1',
+'ballerina/http/Client#post#http://airline-mock-svc:8090',  #reg
+'ballerina/http/Client#post#http://car-mock-svc:8090',      #reg
+'ballerina/http/Client#post#http://hotel-mock-svc:8090',
+'ballerina/http/HttpCachingClient#forward',                 #reg
+'ballerina/http/HttpClient#forward',
+'ballerina/http/HttpClient#get',                            #reg
+'ballerina/http/HttpClient#post']
 
 def run():
 
@@ -16,9 +29,6 @@ def run():
     df = datasets.load_data()
 
     for name, group in df:
-
-        # if name == "ballerina/http/Caller#respond":
-        #     continue
 
         print("\n", name)
 
@@ -50,6 +60,16 @@ def run():
         # results_file.write(str(error))
         # results_file.write("\n")
 
+        data_plot = pd.DataFrame({"api_name": group.api_name, "wip": group.wip, "latency": group.latency, "d_latency": np.polyval(params, group.wip)})
+
+        if name in dominant_apis:
+            sns.lineplot(data=data_plot, x="wip", y="latency")
+            sns.lineplot(data=data_plot, x="wip", y="d_latency")
+            plt.title(name)
+            plt.ylim(ymin=0)
+            plt.xlim(xmin=0)
+            plt.show()
+
     mean_loss = np.mean(loss)
     percentile_loss = np.percentile(loss, 95)
 
@@ -60,7 +80,7 @@ def run():
     # results_file.write("\n95th percentile loss %s" % (percentile_loss)) 
 
 
-# run()
+run()
 
 def predict(api,wip):
     print("[INFO] predicting d_latency...")
