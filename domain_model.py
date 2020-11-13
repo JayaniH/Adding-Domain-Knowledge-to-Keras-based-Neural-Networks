95243ade33517df4b16eabd07571b209fb38bddc
JayaniH <jayani.hewa77@gmail.com>
2020-11-13 09:44:22 +0530

    new domain model

--- domain_model.py
+++ domain_model.py
@@ -21,6 +21,7 @@ dominant_apis = ['ballerina/http/Caller#respond',
 def run():
 
     loss = []
+    equation_params = []
     EPSILON =  1e-6
 
     # results_file = open("./results/domain_results.txt", "w")
@@ -45,14 +46,21 @@ def run():
         params = np.polyfit(trainX, trainY, 2)
         print("Fitteed Parameters: ", params)
 
+        [a, b, c] = params
+        kappa = a /(a + b+  c)
+        sigma = (a + b) / (a + b + c)
+        lambd = 1 / (a + b + c)
+        equation_params.append([kappa, sigma, lambd])
+
         parameters[name] = params
 
         predY = np.polyval(params, testX)
 
-        # rmspe = (np.sqrt(np.mean(np.square((testY - predY) / testY)))) * 100
+        #rmspe
+        error = (np.sqrt(np.mean(np.square((testY - predY) / (testY + EPSILON))))) * 100
         
         # mean absolute percentage error
-        error = np.mean(np.abs((testY - predY) / (testY + EPSILON))) * 100
+        # error = np.mean(np.abs((testY - predY) / (testY + EPSILON))) * 100
         
         print("Loss: ", error)
         loss.append(error)
@@ -60,18 +68,22 @@ def run():
         # results_file.write(str(error))
         # results_file.write("\n")
 
-        data_plot = pd.DataFrame({"api_name": group.api_name, "wip": group.wip, "latency": group.latency, "d_latency": np.polyval(params, group.wip)})
+        # data_plot = pd.DataFrame({"api_name": group.api_name, "wip": group.wip, "latency": group.latency, "d_latency": np.polyval(params, group.wip)})
 
-        if name in dominant_apis:
-            sns.lineplot(data=data_plot, x="wip", y="latency")
-            sns.lineplot(data=data_plot, x="wip", y="d_latency")
-            plt.title(name)
-            plt.ylim(ymin=0)
-            plt.xlim(xmin=0)
-            plt.show()
+        # if name in dominant_apis:
+        #     sns.lineplot(data=data_plot, x="wip", y="latency")
+        #     sns.lineplot(data=data_plot, x="wip", y="d_latency")
+        #     plt.title(name)
+        #     plt.ylim(ymin=0)
+        #     plt.xlim(xmin=0)
+        #     plt.show()
 
     mean_loss = np.mean(loss)
     percentile_loss = np.percentile(loss, 95)
+
+    print("\n".join([str(l) for l in equation_params]), "\n\n")
+
+    print("\n".join([str(l) for l in loss]), "\n\n")
 
     print("Mean loss", mean_loss)
     print("95th percentile loss", percentile_loss)
---
+++ domain_model2.py
@@ -0,0 +1,104 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt 
+import datasets
+
+EPSILON =  1e-6
+
+def fit_with_2deg_up_polynomial_regression_improved(x,y):
+    coeffients = np.polyfit(x, y, 2)
+    if coeffients[0] < 0:
+        #this is not possible as wip increases latency must increase, so we try one degree polynomial 
+        coeffients = np.polyfit(x, y, 1)
+        coeffients = [0, coeffients[0], coeffients[1]]
+        if coeffients[1] < 0:
+            #this is not possible aslo, so we choose a constant function
+            coeffients = [0, 0, np.mean(x)]
+    print(coeffients[0], "x^2 +", coeffients[1], "x +", coeffients[2])
+    polyregressor = np.poly1d(coeffients)
+    # regresson_score = np.sqrt(mean_squared_error(y, polyregressor(x)))
+    regresson_score = (np.sqrt(np.mean(np.square((y - polyregressor(x)) / (y + EPSILON))))) * 100
+    print("polybomial regresson rmspe", regresson_score)    
+    return PolyFit(coeffients, regresson_score)
+
+
+class PolyFit:
+    def __init__(self, coefficents, rmspe):
+        self.coefficents = coefficents
+        self.rmspe = rmspe
+
+
+    def predict(self, x):
+        x = np.array(x)
+        return self.coefficents[0]* x**2 + self.coefficents[1]* x + self.coefficents[2]
+
+
+def run():
+
+    train_loss = []
+    pred_loss = []
+    # equation_params = []
+
+    print("[INFO] loading data...")
+    df = datasets.load_data()
+
+    for name, group in df:
+
+        print("\n", name)
+
+        print("[INFO] constructing training/testing split...")
+        (train, test) = train_test_split(group, test_size=0.3, random_state=42)
+
+        trainX = train["wip"]
+        testX = test["wip"]
+        
+        trainY = train["latency"]
+        testY = test["latency"]
+
+        print("[INFO] fittinig parameters...")
+        model = fit_with_2deg_up_polynomial_regression_improved(trainX, trainY)
+        print("Fitted Parameters: ", model.coefficents)
+
+        train_loss.append(model.rmspe)
+
+        # [a, b, c] = params
+        # kappa = a /(a + b+  c)
+        # sigma = (a + b) / (a + b + c)
+        # lambd = 1 / (a + b + c)
+        # equation_params.append([kappa, sigma, lambd])
+
+        predY = model.predict(testX)
+        # print("Preds : ", predY)
+
+        #rmspe
+        error = (np.sqrt(np.mean(np.square((testY - predY) / (testY + EPSILON))))) * 100
+        
+        # mean absolute percentage error
+        # error = np.mean(np.abs((testY - predY) / (testY + EPSILON))) * 100
+        
+        print("Prediction Loss RMSPE: ", error)
+        pred_loss.append(error)
+
+    mean_train_loss = np.mean(train_loss)
+    percentile_train_loss = np.percentile(train_loss, 95)
+    
+    mean_loss = np.mean(pred_loss)
+    percentile_loss = np.percentile(pred_loss, 95)
+
+    # print("\n".join([str(l) for l in equation_params]), "\n\n")
+    print("\nTraining losses:")
+    print("\n".join([str(l) for l in train_loss]), "\n\n")
+
+    print("Mean train_loss", mean_train_loss)
+    print("95th percentile train_loss", percentile_train_loss)
+
+    print("\nPrediction losses:")
+    print("\n".join([str(l) for l in pred_loss]), "\n\n")
+
+    print("Mean loss", mean_loss)
+    print("95th percentile loss", percentile_loss)
+
+run()
