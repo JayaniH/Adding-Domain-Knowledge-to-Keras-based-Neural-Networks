362feeb0ef051edaecf95b9f0d977700f4b381b3
JayaniH <jayani.hewa77@gmail.com>
2020-11-20 09:15:47 +0530

    regression with 500 epochs, batch size 4

--- regression.py
+++ regression.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt 
+from tensorflow import keras
 from keras import backend as K
 from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 import datasets
 import models
 import numpy as np
@@ -15,14 +17,22 @@ def root_mean_squared_percentage_error(y_true, y_pred):
     EPSILON =  1e-6
     return (K.sqrt(K.mean(K.square((y_true - y_pred) / (y_true + EPSILON))))) * 100
 
+scalerx = MinMaxScaler()
+scalery = MinMaxScaler()
+
 loss = []
 validation_loss = []
-top_5 = [
-    'ballerina/http/Client#post#http://hotel-mock-svc:8090',
-    'ballerina/http/Caller#respond',
-    'ballerina/http/HttpClient#forward',
-    'ballerina/http/HttpClient#post',
+high_loss_apis = [
     'ballerina/http/Client#get#https://covidapi.info/api/v1',
+    'ballerina/http/Client#get#http://postman-echo.com',
+    'ballerina/http/Client#post#http://airline-mock-svc:8090',
+    'ballerina/http/Client#post#http://car-mock-svc:8090',
+    'ballerina/http/Client#post#http://hotel-mock-svc:8090',
+    'ballerina/http/HttpCachingClient#forward',
+    'ballerina/http/Caller#respond',
+    'ballerina/http/HttpClient#get',
+    'ballerina/http/HttpClient#post',
+    'ballerina/http/HttpCachingClient#post'
 ]
 test_apis = [
     'ballerina/http/Client#forward#http://13.90.39.240:8602',
@@ -40,105 +50,132 @@ results_file = open("./results/regression_results.txt", "w")
 print("[INFO] loading data...")
 df = datasets.load_data()
 
-for name, group in df:
 
-    # if name == "ballerina/http/Caller#respond":
-    #     continue
+def train_model():
+    for name, group in df:
 
-    if (name not in top_5) and (name not in test_apis):
-        continue
+        # if name == "ballerina/http/Caller#respond":
+        #     continue
 
-    print(name, "\n")
+        # if (name not in high_loss_apis) and (name not in test_apis):
+        #     continue
+
+        print(name, "\n")
+        
+        results_file.write(name)
+        results_file.write("\n")
+
+        print("[INFO] constructing training/testing split...")
+        (train, test) = train_test_split(group, test_size=0.3, random_state=42)
+
+        print("[INFO] processing data...")
+
+        #scale Y
+        maxLatency = train["latency"].max()
+        trainY = train["latency"] / maxLatency
+        testY = test["latency"] / maxLatency
+
+        # trainY = scalery.fit_transform(train["latency"].values.reshape(-1,1))
+        # testY = scalery.fit_transform(test["latency"].values.reshape(-1,1))
     
-    results_file.write(name)
-    results_file.write("\n")
+        # print(train["latency"], trainY.shape, testY.shape)
 
-    print("[INFO] constructing training/testing split...")
-    (train, test) = train_test_split(group, test_size=0.3, random_state=42)
+        # (trainX, testX) = datasets.process_attributes(train, test)
+        trainX = scalerx.fit_transform(train["wip"].values.reshape(-1,1))
+        testX = scalerx.fit_transform(test["wip"].values.reshape(-1,1))
 
-    #scale Y
-    maxLatency = train["latency"].max()
-    trainY = train["latency"] / maxLatency
-    testY = test["latency"] / maxLatency
+        # print(train["wip"], trainX.shape, testX.shape)
 
-    print("[INFO] processing data...")
-    (trainX, testX) = datasets.process_attributes(train, test)
+        model = models.create_model(trainX.shape[1])
+        opt = Adam(learning_rate=1e-2, decay=1e-3/200)
+        model.compile(loss=root_mean_squared_percentage_error, optimizer=opt)
 
-    model = models.create_model(trainX.shape[1])
-    opt = Adam(learning_rate=1e-2, decay=1e-3/200)
-    model.compile(loss=root_mean_squared_percentage_error, optimizer=opt)
+        print("[INFO] training model...")
+        history = model.fit(x=trainX, y=trainY, validation_data=(testX, testY), epochs=500, batch_size=4)
 
-    print("[INFO] training model...")
-    history = model.fit(x=trainX, y=trainY, validation_data=(testX, testY), epochs=200, batch_size=8)
-    
-    # get final loss
-    loss.append(history.history['loss'][-1])
-    validation_loss.append(history.history['val_loss'][-1])
+        # save model
+        model.save('../models2/' + name.replace("/", "_"))
+        
+        # get final loss
+        loss.append(history.history['loss'][-1])
+        validation_loss.append(history.history['val_loss'][-1])
 
-    # record results
-    print(history.history)
+        # record results
+        # print(history.history)
 
 
-    # results_file.write(str(history.history))
-    # results_file.write("\n\n")
+        # results_file.write(str(history.history))
+        # results_file.write("\n\n")
 
-    # model.evaluate(testX, testY, batch_size=8)
+        # model.evaluate(testX, testY, batch_size=8)
 
-    # plot results
-    # plt.plot(list(range(1,201)), history.history['loss'], label="loss")
-    # plt.title(name)
-    # plt.xlabel('epoch')
-    # plt.ylabel('loss')
-    # plt.ylim(0, 1)
+        #################################
 
-    # plt.plot(list(range(1,201)), history.history['val_loss'], label="val_loss")
-  
-    # plt.legend()
+    print(test_preds_regression)
 
-    # plt.show()
-    # plt.savefig('./Plots/loss/' + name.replace("/", "_") + '_loss.png')
-    # plt.close()
+    mean_loss = np.mean(loss)
+    mean_val_loss = np.mean(validation_loss)
 
-    x = np.arange(0, group.wip.max(), 0.1)
-    # preds = model.predict(x)
-    # test_preds_regression[name] = preds
+    percentile_loss = np.percentile(loss, 95)
+    percentile_val_loss = np.percentile(validation_loss, 95)
 
-    # results = pd.DataFrame({'x': x, 'y_pred': preds})
-    # results_file.write(str(results))
-    # results_file.write(str(preds))
+    print("\n".join([str(l) for l in loss]), "\n\n")
+    print("\n".join([str(l) for l in validation_loss]), "\n\n")
+
+    print("Mean loss/val_loss", mean_loss, mean_val_loss)
+    print("95th percentile loss/val_loss", percentile_loss, percentile_val_loss)
+
+    # results_file.write("\nMean loss/val_loss %s / %s" % (mean_loss, mean_val_loss)) 
+    # results_file.write("\n95th percentile loss/val_loss %s / %s" % (percentile_loss, percentile_val_loss)) 
+
+    results_file.write("\ntop 5")
+    results_file.write(str(top_5_preds))
+    results_file.write("\ntest")
+    results_file.write(str(test_preds_regression))
+    results_file.close()
 
 
-    if name in top_5:
-        preds = model.predict(x)
-        top_5_preds[name] = preds
-        # print('preds', name, preds)
+def run_regression():
+    for name, group in df:
 
-    if name in test_apis:
-        preds = model.predict(x)
+        # if name == "ballerina/http/Caller#respond":
+        #     continue
+
+        (train, test) = train_test_split(group, test_size=0.3, random_state=42)
+
+        maxLatency = train["latency"].max()
+
+        model = keras.models.load_model('../models2/' + name.replace("/", "_"), compile=False)
+        x = np.arange(0, group.wip.max() +1 , 0.1)
+        preds = model.predict(scalerx.fit_transform(x.reshape(-1, 1)))
+        preds = preds * maxLatency
         test_preds_regression[name] = preds
-        # print('preds', name, preds)
+        print(preds)
+
+        # results_file.write(str(preds))
+
+        # if name in high_loss_apis:
+        #     top_5_preds[name] = preds
+
+        # if name in test_apis:
+        #     test_preds_regression[name] = preds
+
+        plt.scatter(group.wip, group.latency)
+        plt.plot(x, preds, 'r', label='regression')
+        plt.yscale("log")
+        plt.title(name)
+        plt.xlabel('wip')
+        plt.ylabel('latency')
+        plt.legend()
+        plt.show()
+        # plt.savefig('../Plots/scale_y_div_max/' + name.replace("/", "_") + '_loss.png')
+        plt.close()
 
 
-
-print(test_preds_regression)
-
-mean_loss = np.mean(loss)
-mean_val_loss = np.mean(validation_loss)
-
-percentile_loss = np.percentile(loss, 95)
-percentile_val_loss = np.percentile(validation_loss, 95)
-
-print("Mean loss/val_loss", mean_loss, mean_val_loss)
-print("95th percentile loss/val_loss", percentile_loss, percentile_val_loss)
-
-# results_file.write("\nMean loss/val_loss %s / %s" % (mean_loss, mean_val_loss)) 
-# results_file.write("\n95th percentile loss/val_loss %s / %s" % (percentile_loss, percentile_val_loss)) 
-
-results_file.write("\ntop 5")
-results_file.write(str(top_5_preds))
-results_file.write("\ntest")
-results_file.write(str(test_preds_regression))
-results_file.close()
 
 def regression_forecast():
     return top_5_preds, test_preds_regression
+
+
+train_model()
+# run_regression()
