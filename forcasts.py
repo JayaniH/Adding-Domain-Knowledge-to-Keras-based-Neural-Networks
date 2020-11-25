12b275980a64f76f328162594e0645b6ee84d726
JayaniH <jayani.hewa77@gmail.com>
2020-11-25 16:19:43 +0530

    forecasts - code cleanup

--- forcasts.py
+++ forcasts.py
@@ -2,29 +2,28 @@ import numpy as np
 import pandas as pd
 import regression
 import regression_xgboost
-import domain_model2
 import domain_model3
 import seaborn as sns
 import matplotlib.pyplot as plt 
 
 high_loss_apis = [
-    'ballerina/http/Client#get#https://covidapi.info/api/v1',
+    'ballerina/http/Client#post#http://hotel-mock-svc:8090',
     'ballerina/http/Client#get#http://postman-echo.com',
+    'ballerina/http/Client#get#https://covidapi.info/api/v1',
     'ballerina/http/Client#post#http://airline-mock-svc:8090',
     'ballerina/http/Client#post#http://car-mock-svc:8090',
-    'ballerina/http/Client#post#http://hotel-mock-svc:8090',
-    'ballerina/http/HttpCachingClient#forward',
-    'ballerina/http/Caller#respond',
-    'ballerina/http/HttpClient#get',
+    'ballerina/http/HttpClient#forward',
+    # 'ballerina/http/Caller#respond',
     'ballerina/http/HttpClient#post',
+    'ballerina/http/HttpClient#get',
     'ballerina/http/HttpCachingClient#post'
 ]
 test_apis = [
-    'ballerina/http/Client#forward#http://13.90.39.240:8602',
     'ballerina/http/Client#get#https://ap15.salesforce.com',
-    'ballerina/http/Client#patch#https://graph.microsoft.com',
+    'ballerina/http/Client#post#https://ap15.salesforce.com',
     'ballerina/http/Client#post#https://login.salesforce.com/services/oauth2/token',
-    'ballerinax/sfdc/SObjectClient#createRecord'
+    'ballerinax/sfdc/QueryClient#getQueryResult',
+    'ballerinax/sfdc/SObjectClient#createOpportunity'
 ]
 
 
@@ -37,11 +36,11 @@ df2 = df[df['api_name'].isin(test_apis)]
 regression.run_regression()
 domain_model3.run()
 
-xgboost_top5_preds, xgboost_test_preds = regression_xgboost.xgb_regression_forecast()
+xgboost_test_preds = regression_xgboost.xgb_regression_forecast()
 
-regression_top5__preds, regression_test_preds = regression.regression_forecast()
+regression_test_preds = regression.regression_forecast()
 
-domain_test_preds = domain_model2.domain_forecast()
+domain_test_preds = domain_model3.domain_forecast()
 
 # data_plot = pd.DataFrame({"api_name":high_loss_apis, "wip": df.wip, "latency": df.latency, "xgboost": xgboost_preds, "domain": domain_preds, "wip_all": np.arange(0, 1500)})
 # g = sns.FacetGrid(data_plot, col="api_name", col_wrap=5)
@@ -60,79 +59,35 @@ df2 = df2.groupby(by="api_name")
 
 results_file = open("./results/predictions.txt", "w")
 
-# print("Top 5 \n")
-# results_file.write("\n\n Top 10 APIs predictions")
-# for name, group in df1:
-
-#     results_file.write(name)
-#     results_file.write("\n")
-#     results_file.write(str(regression_test_preds[name]))
-#     results_file.write("\n")
-
-#     x = np.arange(0, group.wip.max() +1 , 0.1)
-
-#     plt.scatter(group.wip, group.latency)
-#     plt.plot(x, regression_test_preds[name], 'r', label='regression')
-#     plt.plot(np.arange(0, int(group.wip.max()) + 1, 0.1), xgboost_test_preds[name], 'm', label='xgboost')
-#     plt.plot(x, domain_test_preds[name], 'y', label='domain')
-#     plt.yscale("log")
-#     plt.title(name)
-#     plt.xlabel('wip')
-#     plt.ylabel('latency')
-#     plt.legend()
-#     # plt.show()
-#     plt.savefig('../Plots/forecasts/' + name.replace("/", "_") + '_highloss.png')
-#     plt.close()
-
-# print("Test \n")
-# results_file.write("\n\n Test APIs predictions")
-# for name, group in df2:
-
-#     results_file.write(name)
-#     results_file.write("\n")
-#     results_file.write(str(regression_test_preds[name]))
-#     results_file.write("\n")
-
-#     x = np.arange(0, group.wip.max() +1 , 0.1)
-
-#     plt.scatter(group.wip, group.latency)
-#     plt.plot(x, regression_test_preds[name], 'r', label='regression')
-#     plt.plot(np.arange(0, int(group.wip.max()) + 1, 0.1), xgboost_test_preds[name], 'm', label='xgboost')
-#     plt.plot(x, domain_test_preds[name], 'y', label='domain')
-#     plt.yscale("log")
-#     plt.title(name)
-#     plt.xlabel('wip')
-#     plt.ylabel('latency')
-#     plt.legend()
-#     # plt.show()
-#     plt.savefig('../Plots/forecasts/' + name.replace("/", "_") + '_test.png')
-#     plt.close()
-
-
 df = df.groupby(by="api_name")
 i = 0
 
 for name, group in df:
+
+    if (name not in high_loss_apis) and (name not in test_apis):
+        continue
 
     results_file.write(name)
     results_file.write("\n")
     results_file.write(str(regression_test_preds[name]))
     results_file.write("\n")
 
-    x = np.arange(0, group.wip.max() +1 , 0.1)
+    x = np.arange(0, group.wip.max() + 0.1 , 0.01)
+    print(xgboost_test_preds[name])
 
     # plt.yscale("log")
     plt.scatter(group.wip, group.latency)
     # plt.plot(x, regression_test_preds[name], 'r', label='regression')
-    plt.plot(np.arange(0, int(group.wip.max()) + 1, 0.1), xgboost_test_preds[name], 'm', label='xgboost')
-    plt.plot(x, domain_test_preds[name], 'y', label='domain')
+    plt.plot(x, xgboost_test_preds[name], 'm', label='xgboost')
+    # plt.plot(x, domain_test_preds[name], 'y', label='domain')
     plt.title(name)
     plt.xlabel('wip')
     plt.ylabel('latency')
+    plt.ylim(ymin=0)
     plt.legend()
     # plt.figtext(0.5, 0, "regression val_loss: " + str(val_loss[i]), fontsize=11)
-    # plt.show()
-    plt.savefig('../Plots/domain_plots/' + name.replace("/", "_") + '.png')
+    plt.show()
+    # plt.savefig('../Plots/forecasts_new/' + name.replace("/", "_") + '.png')
     plt.close()
     i=i+1
 
