0247f016f0831d86cda01eae5b59223e32c37cc1
JayaniH <jayani.hewa77@gmail.com>
2020-12-04 10:45:07 +0530

    regression without scalling Y

--- regression.py
+++ regression.py
@@ -21,6 +21,7 @@ def root_mean_squared_percentage_error(y_true, y_pred):
 
 loss = []
 validation_loss = []
+prediction_loss = []
 sample_loss = []
 high_loss_apis = [
     'ballerina/http/Client#post#http://hotel-mock-svc:8090',
@@ -48,7 +49,7 @@ print("[INFO] loading data...")
 df = datasets.load_data()
 
 
-def train_model():
+def train_models():
     results_file = open("./results/regression_results.txt", "w")
 
     for name, group in df:
@@ -61,6 +62,8 @@ def train_model():
 
         group = datasets.remove_outliers(group)
 
+        # maxLatency = group["latency"].max()
+
         print(name, "\n")
         
         results_file.write(name)
@@ -72,9 +75,8 @@ def train_model():
         print("[INFO] processing data...")
 
         #scale Y
-        maxLatency = train["latency"].max()
-        trainY = train["latency"] / maxLatency
-        testY = test["latency"] / maxLatency
+        trainY = train["latency"] #/ maxLatency
+        testY = test["latency"] #/ maxLatency
     
         # scale X
         # (trainX, testX) = datasets.process_attributes(train, test)
@@ -86,7 +88,7 @@ def train_model():
 
         # save scaler
 
-        outfile = open("../models/rmse_outliers_removed/_scalars/scaler_" + name.replace("/", "_") + ".pkl", "wb")
+        outfile = open("../models/12_regression_epoch200_batch4_rmse_ouliers_removed_unscaledY/_scalars/scaler_" + name.replace("/", "_") + ".pkl", "wb")
         pkl.dump(scalerx, outfile)
         outfile.close()
 
@@ -95,14 +97,20 @@ def train_model():
         model.compile(loss=root_mean_squared_error, optimizer=opt)
 
         print("[INFO] training model...")
-        history = model.fit(x=trainX, y=trainY, validation_data=(testX, testY), epochs=500, batch_size=4)
+        history = model.fit(x=trainX, y=trainY, validation_data=(testX, testY), epochs=200, batch_size=4)
 
         # save model
-        model.save('../models/rmse_outliers_removed/' + name.replace("/", "_"))
+        model.save('../models/12_regression_epoch200_batch4_rmse_ouliers_removed_unscaledY/' + name.replace("/", "_"))
         
         # get final loss
-        loss.append(history.history['loss'][-1] * maxLatency)
-        validation_loss.append(history.history['val_loss'][-1] * maxLatency)
+        loss.append(history.history['loss'][-1]) # * maxLatency
+        validation_loss.append(history.history['val_loss'][-1] ) # * maxLatency
+
+        # evaluation with entire test set
+        predY = model.predict(testX)
+        # mae = np.mean(np.abs(testY.values - predY))
+        rmse = np.sqrt(np.mean(np.square(testY.values - predY)))
+        prediction_loss.append(rmse) # * maxLatency
 
         # evaluation using bucket method
         error = []
@@ -110,7 +118,7 @@ def train_model():
         for i in range(5):
             test_sample = datasets.get_test_sample(test)
             testX = scalerx.transform(test_sample["wip"].values.reshape(-1,1))
-            testY = test_sample["latency"] / maxLatency
+            testY = test_sample["latency"] #/ maxLatency
 
             predY = model.predict(testX)
             # mae = np.mean(np.abs(testY.values - predY))
@@ -121,12 +129,7 @@ def train_model():
 
         avg_error = np.mean(error)
         print("Loss: ", avg_error)
-        sample_loss.append(avg_error * maxLatency)
-
-        # preds for dataset
-        # pred_y = model.predict(testX, batch_size=4)
-        # print("Test RMSE", np.sqrt(np.mean(np.square(testY.values - pred_y))) * maxLatency)
-        # pred_y = pred_y * maxLatency
+        sample_loss.append(avg_error) # * maxLatency
 
         # record results
         # print(history.history)
@@ -138,18 +141,27 @@ def train_model():
 
     mean_loss = np.mean(loss)
     mean_val_loss = np.mean(validation_loss)
+    mean_prediction_loss = np.mean(prediction_loss)
     mean_sample_loss = np.mean(sample_loss)
+
+    median_loss = np.percentile(loss, 50)
+    median_val_loss = np.percentile(validation_loss, 50)
+    median_prediction_loss = np.percentile(prediction_loss, 50)
+    median_sample_loss = np.percentile(sample_loss, 50)
 
     percentile_loss = np.percentile(loss, 95)
     percentile_val_loss = np.percentile(validation_loss, 95)
+    percentile_prediction_loss = np.percentile(prediction_loss, 95)
     percentile_sample_loss = np.percentile(sample_loss, 95)
 
     print("\n".join([str(l) for l in loss]), "\n\n")
     print("\n".join([str(l) for l in validation_loss]), "\n\n")
+    print("\n".join([str(l) for l in prediction_loss]), "\n\n")
     print("\n".join([str(l) for l in sample_loss]), "\n\n")
 
-    print("Mean loss/val_loss/sample_loss", mean_loss, mean_val_loss, mean_sample_loss)
-    print("95th percentile loss/val_loss/sample_loss", percentile_loss, percentile_val_loss, percentile_sample_loss)
+    print("Mean loss/val_loss/prediction_loss/sample_loss", mean_loss, mean_val_loss, mean_prediction_loss, mean_sample_loss)
+    print("Median loss/val_loss/prediction_loss/sample_loss/", median_loss, median_val_loss, median_prediction_loss, median_sample_loss)
+    print("95th percentile loss/val_loss/prediction_loss/sample_loss", percentile_loss, percentile_val_loss, percentile_prediction_loss, percentile_sample_loss)
 
     # results_file.write("\nMean loss/val_loss %s / %s" % (mean_loss, mean_val_loss)) 
     # results_file.write("\n95th percentile loss/val_loss %s / %s" % (percentile_loss, percentile_val_loss)) 
@@ -174,40 +186,32 @@ def evaluate_models():
 
         group = datasets.remove_outliers(group)
 
-        infile = open("../models/rmse_outliers_removed/_scalars/scaler_" + name.replace("/", "_") + ".pkl", "rb")
+        # maxLatency = group["latency"].max()
+
+        infile = open("../models/12_regression_epoch200_batch4_rmse_ouliers_removed_unscaledY/_scalars/scaler_" + name.replace("/", "_") + ".pkl", "rb")
         scalerx = pkl.load(infile)
         infile.close()
 
         (train, test) = train_test_split(group, test_size=0.3, random_state=42)
 
-        maxLatency = train["latency"].max()
-
-        model = keras.models.load_model('../models/rmse_outliers_removed/' + name.replace("/", "_"), compile=False)
+        model = keras.models.load_model('../models/12_regression_epoch200_batch4_rmse_ouliers_removed_unscaledY/' + name.replace("/", "_"), compile=False)
 
         # preds for regression curve
         x = np.arange(0, group.wip.max() + 0.1 , 0.01)
         preds = model.predict(scalerx.transform(x.reshape(-1, 1)))
-        preds = preds * maxLatency
+        preds = preds # * maxLatency
 
         # print(name, preds)
 
-        # preds for dataset
-        testX = scalerx.transform(test["wip"].values.reshape(-1,1))
-        testY = test["latency"] / maxLatency
-
-        pred_y = model.predict(testX, batch_size=4)
-        # error.append(np.mean(np.abs(testY.values - pred_y)))
-        pred_y = pred_y * maxLatency
-        # error.append(np.mean(np.abs(test["latency"].values - pred_y)))
-
         # evaluation with entire test set
         testX = scalerx.transform(test["wip"].values.reshape(-1,1))
-        testY = test["latency"] / maxLatency
+        testY = test["latency"] # / maxLatency
 
         predY = model.predict(testX)
+        pred_y = predY # * maxLatency
         # mae = np.mean(np.abs(testY.values - predY))
         rmse = np.sqrt(np.mean(np.square(testY.values - predY)))
-        validation_loss.append(rmse * maxLatency)
+        prediction_loss.append(rmse) # * maxLatency
         
         # evaluation using bucket method
         error = []
@@ -215,7 +219,7 @@ def evaluate_models():
         for i in range(5):
             test_sample = datasets.get_test_sample(test)
             testX = scalerx.transform(test_sample["wip"].values.reshape(-1,1))
-            testY = test_sample["latency"] / maxLatency
+            testY = test_sample["latency"] # / maxLatency
 
             predY = model.predict(testX)
             # mae = np.mean(np.abs(testY.values - predY))
@@ -226,7 +230,7 @@ def evaluate_models():
 
         avg_error = np.mean(error)
         print("Sample Loss: ", avg_error)
-        sample_loss.append(avg_error * maxLatency)
+        sample_loss.append(avg_error) # maxLatency
 
         # plt.yscale("log")
         plt.scatter(group.wip, group.latency, label='data')
@@ -236,21 +240,25 @@ def evaluate_models():
         plt.xlabel('wip')
         plt.ylabel('latency')
         plt.legend()
-        # plt.show()
+        plt.show()
         # plt.savefig('../Plots/regression_test_plots_mae_outliers_removed/' + name.replace("/", "_") + '_loss.png')
         plt.close()
 
-    mean_val_loss = np.mean(validation_loss)
+    mean_prediction_loss = np.mean(prediction_loss)
     mean_sample_loss = np.mean(sample_loss)
 
-    percentile_val_loss = np.percentile(validation_loss, 95)
+    median_prediction_loss = np.percentile(prediction_loss, 50)
+    median_sample_loss = np.percentile(sample_loss, 50)
+
+    percentile_prerdiction_loss = np.percentile(prediction_loss, 95)
     percentile_sample_loss = np.percentile(sample_loss, 95)
 
-    print("\n".join([str(l) for l in validation_loss]), "\n\n")
+    print("\n".join([str(l) for l in prediction_loss]), "\n\n")
     print("\n".join([str(l) for l in sample_loss]), "\n\n")
 
-    print("Mean val_loss/sample_loss", mean_val_loss, mean_sample_loss)
-    print("95th percentile val_loss/sample_loss", percentile_val_loss, percentile_sample_loss)
+    print("Mean prediction_loss/sample_loss", mean_prediction_loss, mean_sample_loss)
+    print("Median loss/prediction_loss/sample_loss", median_prediction_loss, median_sample_loss)
+    print("95th percentile prediction_loss/sample_loss", percentile_prerdiction_loss, percentile_sample_loss)
 
 
 
@@ -258,22 +266,22 @@ def get_regression_forecasts():
     for name, group in df:
 
         group = datasets.remove_outliers(group)
-        maxLatency = group["latency"].max()
+        # maxLatency = group["latency"].max()
 
-        infile = open("../models/rmse_outliers_removed/_scalars/scaler_" + name.replace("/", "_") + ".pkl", "rb")
+        infile = open("../models/12_regression_epoch200_batch4_rmse_ouliers_removed_unscaledY/_scalars/scaler_" + name.replace("/", "_") + ".pkl", "rb")
         scalerx = pkl.load(infile)
         infile.close()
 
-        model = keras.models.load_model('../models/rmse_outliers_removed/' + name.replace("/", "_"), compile=False)
+        model = keras.models.load_model('../models/12_regression_epoch200_batch4_rmse_ouliers_removed_unscaledY/' + name.replace("/", "_"), compile=False)
 
         # preds for regression curve
         x = np.arange(0, group.wip.max() + 0.1 , 0.01)
         preds = model.predict(scalerx.transform(x.reshape(-1, 1)))
-        preds = preds * maxLatency
+        preds = preds # * maxLatency
         regression_predictions[name] = preds
 
         # print(name, preds)
     return regression_predictions
 
-# train_model()
+train_models()
 # evaluate_models()
