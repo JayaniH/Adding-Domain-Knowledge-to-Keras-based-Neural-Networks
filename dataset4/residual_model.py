cabb71c603ac1ccb983902b9eb2b8062d3deaa77
JayaniH <jayani.hewa77@gmail.com>
2021-03-17 13:24:00 +0530

    dataset 4 - residual model

--- dataset4/residual_model.py
+++ dataset4/residual_model.py
@@ -5,8 +5,9 @@ from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import MinMaxScaler
-import models
 import _helpers
+import models
+import domain_model
 import numpy as np
 import pandas as pd
 import pickle as pkl
@@ -20,7 +21,12 @@ def root_mean_squared_error(y_true, y_pred):
 print('[INFO] loading data...')
 df = pd.read_csv('springboot_db_truncated.csv', sep=',')
 
+domain_model_parameters = domain_model.get_parameters()
+
 def train_model(train_i, test_i, i):
+        
+    df['domain_prediction'] = domain_model.predict(df['concurrent_users'], domain_model_parameters[i])
+    df['residual'] = df['domain_prediction'] - df['latency']
 
     print('[INFO] processing data...')
 
@@ -29,6 +35,11 @@ def train_model(train_i, test_i, i):
 
     trainY = train['latency']
     testY = test['latency']
+
+    #scaling
+
+    trainY = train['residual']
+    testY = test['residual']
 
     scalerX = MinMaxScaler()
     trainX = scalerX.fit_transform(train[['concurrent_users', 'heap_size', 'collector']].values.reshape(-1,3))
@@ -39,8 +50,8 @@ def train_model(train_i, test_i, i):
     pkl.dump(scalerX, outfile)
     outfile.close()
 
-    model = models.create_model(trainX.shape[1])
-    opt = Adam(learning_rate=0.01)
+    model = models.create_residual_model(trainX.shape[1])
+    opt = Adam(learning_rate=1e-2, decay=1e-3/200)
     model.compile(loss=root_mean_squared_error, optimizer=opt)
 
     print('[INFO] training model...')
@@ -52,16 +63,28 @@ def train_model(train_i, test_i, i):
     loss = history.history['loss'][-1]
     validation_loss = history.history['val_loss'][-1]
 
-    print('[INFO] predicting latency...')
-    latency_prediction = model.predict(testX)
-    
+    print('[INFO] predicting residuals...')
+    residual_prediction = model.predict(testX)
+
+    # residual_prediction = residual = domain_prediction - latency
+    # latency_prediction = domain_prediction  - (domain_prediction - latency)
+
+    print('[INFO] calculating latency predictions...')
+    latency_prediction = test['domain_prediction'] - residual_prediction.flatten()
+
+    _helpers.print_predictions(test['latency'].values, latency_prediction.values)
+
     print('[RESULT] loss / val_loss', loss, validation_loss)
-    rmse, mae, mape = _helpers.get_error(testY.values, latency_prediction.flatten())   
+    rmse, mae, mape = _helpers.get_error(test['latency'].values, latency_prediction)
+
+    save_predictions_to_csv(i, test, residual_prediction.flatten(), latency_prediction)
 
     return rmse, mae, mape
 
-
 def evaluate_model(train_i, test_i, i):
+    
+    df['domain_prediction'] = domain_model.predict(df['concurrent_users'], domain_model_parameters[i])
+    df['residual'] = df['domain_prediction'] - df['latency']
 
     infile = open('../../models/springboot/new_model/_scalars/scalerX_' + str(i+1) +'.pkl', 'rb')
     scalerX = pkl.load(infile)
@@ -74,17 +97,25 @@ def evaluate_model(train_i, test_i, i):
 
     # preds for dataset
     testX = scalerX.transform(test[['concurrent_users', 'heap_size', 'collector']].values.reshape(-1,3))
-    testY = test['latency']
+    testY = test['residual']
 
-    latency_prediction = model.predict(testX)
+    print('[INFO] predicting residuals...')
+    residual_prediction = model.predict(testX)
 
-    _helpers.print_predictions(testY.values, latency_prediction.flatten())
+    # residual_prediction = d_latency  - (d_latency - latency)
+    # latency_prediction = test['domain_prediction'] - scalerY.inverse_transform(residual_prediction).flatten()
 
-    results_df = pd.DataFrame({'concurrent_users': test['concurrent_users'], 'heap_size': test['heap_size'], 'collector': test['collector'], 'latency': testY, 'prediction': latency_prediction.flatten()})
-    print(results_df)
-    results_df.to_csv('../../models/springboot/new_model/results/K' + str(i+1) + '.csv', sep=",", index= False)
+    print('[INFO] calculating latency predictions...')
+    latency_prediction = test['domain_prediction'] - residual_prediction.flatten()
 
-    rmse, mae, mape = _helpers.get_error(testY.values, latency_prediction.flatten())
+    # remove negative preds
+    # latency_prediction = np.maximum(0.0, latency_prediction)
+
+    _helpers.print_predictions(test['latency'].values, latency_prediction.values)
+
+    rmse, mae, mape = _helpers.get_error(test['latency'].values, latency_prediction)
+
+    save_predictions_to_csv(i, test, residual_prediction.flatten(), latency_prediction)
     
     return rmse, mae, mape
 
@@ -109,7 +140,6 @@ def train_with_cross_validation():
 
     return errors
 
-
 def evaluate_with_cross_validation():
     errors = {'rmse' : [], 'mae': [], 'mape': []}
     
@@ -124,12 +154,18 @@ def evaluate_with_cross_validation():
         errors['rmse'].append(rmse)
         errors['mae'].append(mae)
         errors['mape'].append(mape)
+
         i += 1
 
     _helpers.print_errors(errors)
-
     return errors
 
+
+# helper functions
+def save_predictions_to_csv(i, df, residual_prediction, latency_prediction):
+    results_df = pd.DataFrame({'concurrent_users': df['concurrent_users'], 'heap_zize': df['heap_size'], 'collector': df['collector'], 'latency': df['latency'], 'domain_prediction': df['domain_prediction'],'residual': df['residual'] , 'residual_prediction': residual_prediction, 'prediction': latency_prediction})
+    print(results_df)
+    results_df.to_csv('../../models/springboot/new_model/results/K' + str(i+1) + '.csv', sep=",", index= False)
 
 train_with_cross_validation()
 # evaluate_with_cross_validation()
