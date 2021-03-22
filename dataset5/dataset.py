d07bde3f3a62b558ed24814f99af58dac29d39d6
JayaniH <jayani.hewa77@gmail.com>
2021-03-22 16:40:50 +0530

    dataset 5 - ml model

---
+++ dataset5/dataset.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def truncate_dataset(infile, outfile):
+    df = pd.read_csv(infile, sep=',')
+    df = df[['use case', 'size', 'heap', 'user', 'collector', 'average_latency']]
+    df = df.rename(columns={'use case': 'use_case', 'heap': 'heap_size', 'user': 'concurrent_users', 'collector': 'collector', 'average_latency': 'latency'})
+    df = df.replace({
+        'use_case': {'Merge': 1, 'Echo': 2},
+        'heap_size': {'100m': 100, '200m': 200, '500m': 500, '1g': 1000, '4g': 4000}, 
+        'collector': {'UseSerialGC': 1, 'UseParallelGC': 2, 'UseG1GC': 3, 'UseConcMarkSweepGC': 4}
+        })
+    print(df)
+    df.to_csv(outfile, sep=',', index= False)
+
+def plot(file):
+    df = pd.read_csv(file, sep=',')
+    df = remove_outliers(df)
+    plt.scatter(df['concurrent_users'], df['latency'])
+    plt.show()
+
+def remove_outliers(df):
+    n = int(max(df['concurrent_users']))+1
+
+    k = 1.4826                 # scale factor for Gaussian distribution
+
+    window_size = n/10
+    n_sigma = 4  
+
+    filtered_df = pd.DataFrame()
+    outlier_df = pd.DataFrame()
+    
+    for i in np.arange(window_size,n+2,window_size):
+        windowed_df = df[(df['concurrent_users']>(i - window_size)) & (df['concurrent_users']<=(i))]
+        if len(windowed_df)>2:
+            x0 = np.median(windowed_df['latency'])
+            S0 = k * np.median(np.abs(windowed_df['latency'] - x0))         # median absolute deviation
+            # S0 = windowed_df['latency'].std()                             # standard deviation
+            filtered_window = windowed_df[np.abs(windowed_df['latency'] - x0) < n_sigma * S0]
+            outlier_window = pd.concat([windowed_df,filtered_window]).drop_duplicates(keep=False)
+            filtered_df = filtered_df.append([filtered_window], ignore_index=True)
+            outlier_df = outlier_df.append([outlier_window], ignore_index=True)
+            # print (windowed_df.shape, filtered_window.shape, outlier_window.shape)
+        else:
+            filtered_df = filtered_df.append([windowed_df], ignore_index=True)
+
+    return filtered_df
+
+# truncate_dataset('springboot_summary.csv', 'springboot_summary_truncated.csv')
+# plot('springboot_summary_truncated.csv')
--- dataset5/ml_model.py
+++ dataset5/ml_model.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import KFold
 from sklearn.preprocessing import MinMaxScaler
 import models
 import _helpers
+import dataset
 import numpy as np
 import pandas as pd
 import pickle as pkl
@@ -18,13 +19,14 @@ def root_mean_squared_error(y_true, y_pred):
 # load data
 print('[INFO] loading data...')
 df = pd.read_csv('springboot_summary_truncated.csv', sep=',')
+df = dataset.remove_outliers(df)
 
-def train_model():
-
-    print('[INFO] constructing training/testing split...')
-    (train, test) = train_test_split(df, test_size=0.3, random_state=42)
+def train_model(train_i, test_i, i):
 
     print('[INFO] processing data...')
+
+    train = df.iloc[train_i]
+    test = df.iloc[test_i]
 
     trainY = train['latency']
     testY = test['latency']
@@ -34,19 +36,19 @@ def train_model():
     testX = scalerX.transform(test[['concurrent_users', 'heap_size', 'collector', 'size',  'use_case']].values.reshape(-1,5))
 
     # save scaler X
-    outfile = open('../../models/springboot2/new_model/_scalars/scalerX.pkl', 'wb')
+    outfile = open('../../models/springboot2/new_model/_scalars/scalerX_' + str(i+1) +'.pkl', 'wb')
     pkl.dump(scalerX, outfile)
     outfile.close()
 
     model = models.create_model(trainX.shape[1])
-    opt = Adam(learning_rate=0.01)
+    opt = Adam(learning_rate=0.001)
     model.compile(loss=root_mean_squared_error, optimizer=opt)
 
     print('[INFO] training model...')
     history = model.fit(x=trainX, y=trainY, validation_data=(testX, testY), epochs=200, batch_size=4)
 
     # save model
-    model.save('../../models/springboot2/new_model/model')
+    model.save('../../models/springboot2/new_model/K' + str(i+1))
 
     loss = history.history['loss'][-1]
     validation_loss = history.history['val_loss'][-1]
@@ -60,33 +62,77 @@ def train_model():
     return rmse, mae, mape
 
 
-def evaluate_model():
+def evaluate_model(train_i, test_i, i):
 
-    infile = open('../../models/springboot2/new_model/_scalars/scalerX.pkl', 'rb')
+    infile = open('../../models/springboot2/new_model/_scalars/scalerX_' + str(i+1) +'.pkl', 'rb')
     scalerX = pkl.load(infile)
     infile.close()
 
-    print('[INFO] constructing training/testing split...')
-    (train, test) = train_test_split(df, test_size=0.3, random_state=42)
+    train = df.iloc[train_i]
+    test = df.iloc[test_i]
 
-    model = keras.models.load_model('../../models/springboot2/new_model/model', compile=False)
+    model = keras.models.load_model('../../models/springboot2/new_model/K' + str(i+1), compile=False)
 
     # preds for dataset
     testX = scalerX.transform(test[['concurrent_users', 'heap_size', 'collector', 'size',  'use_case']].values.reshape(-1,5))
     testY = test['latency']
 
     latency_prediction = model.predict(testX)
+    # remove negative preds
+    latency_prediction = np.maximum(0.0, latency_prediction)
 
-    _helpers.print_predictions(testY.values, latency_prediction.flatten())
+    # _helpers.print_predictions(testY.values, latency_prediction.flatten())
 
     results_df = pd.DataFrame({'concurrent_users': test['concurrent_users'], 'heap_size': test['heap_size'], 'collector': test['collector'], 'latency': testY, 'prediction': latency_prediction.flatten()})
-    print(results_df)
-    results_df.to_csv('../../models/springboot2/new_model/results/result.csv', sep=",", index= False)
+    # print(results_df)
+    results_df.to_csv('../../models/springboot2/new_model/results/K' + str(i+1) + '.csv', sep=",", index= False)
 
     rmse, mae, mape = _helpers.get_error(testY.values, latency_prediction.flatten())
     
     return rmse, mae, mape
 
 
-# train_model()
-evaluate_model()
+def train_with_cross_validation():
+    errors = {'rmse' : [], 'mae': [], 'mape': []}
+
+    print('[INFO] constructing k fold split...')
+    kf = KFold(n_splits=10, shuffle = True, random_state=14)
+    i = 0
+
+    for train_i, test_i in kf.split(df):
+        print('--------------------------------------------------------------------------------------------------------------------------------')
+        print('K', str(i+1), '\n')
+        rmse, mae, mape = train_model(train_i, test_i, i)
+        errors['rmse'].append(rmse)
+        errors['mae'].append(mae)
+        errors['mape'].append(mape)
+        i += 1
+
+    _helpers.print_errors(errors)
+
+    return errors
+
+
+def evaluate_with_cross_validation():
+    errors = {'rmse' : [], 'mae': [], 'mape': []}
+    
+    print('[INFO] constructing k fold split...')
+    kf = KFold(n_splits=10, shuffle = True, random_state=14)
+    i = 0
+
+    for train_i, test_i in kf.split(df):
+        print('--------------------------------------------------------------------------------------------------------------------------------')
+        print('\nK', i+1)
+        rmse, mae, mape = evaluate_model(train_i, test_i, i)
+        errors['rmse'].append(rmse)
+        errors['mae'].append(mae)
+        errors['mape'].append(mape)
+        i += 1
+
+    _helpers.print_errors(errors)
+
+    return errors
+
+
+# train_with_cross_validation()
+evaluate_with_cross_validation()
