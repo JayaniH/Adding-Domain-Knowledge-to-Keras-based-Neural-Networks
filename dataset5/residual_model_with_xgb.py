c56691b3db7a27cd0b1d4dbfad2735dfe41fca73
JayaniH <jayani.hewa77@gmail.com>
2021-04-05 13:42:21 +0530

    dataset 5 - residual models with ml models

---
+++ dataset5/residual_model_with_xgb.py
@@ -0,0 +1,171 @@
+import matplotlib.pyplot as plt 
+from tensorflow import keras
+from keras import backend as K
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import MinMaxScaler
+import _helpers
+import models
+import dataset
+import xgb
+import numpy as np
+import pandas as pd
+import pickle as pkl
+import random
+
+
+def root_mean_squared_error(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred - y_true)))
+
+# load data
+print('[INFO] loading data...')
+df = pd.read_csv('springboot_summary_truncated.csv', sep=',')
+df = dataset.remove_outliers(df)
+
+def train_model(train_i, test_i, i):
+        
+    df['xgb_prediction'] = xgb.predict(i, df[['concurrent_users', 'heap_size', 'collector', 'size',  'use_case']].values.reshape(-1,5))
+    df['residual'] = df['xgb_prediction'] - df['latency']
+
+    print('[INFO] processing data...')
+
+    train = df.iloc[train_i]
+    test = df.iloc[test_i]
+
+    trainY = train['latency']
+    testY = test['latency']
+
+    #scaling
+
+    trainY = train['residual']
+    testY = test['residual']
+
+    scalerX = MinMaxScaler()
+    trainX = scalerX.fit_transform(train[['concurrent_users', 'heap_size', 'collector', 'size',  'use_case']].values.reshape(-1,5))
+    testX = scalerX.transform(test[['concurrent_users', 'heap_size', 'collector', 'size',  'use_case']].values.reshape(-1,5))
+
+    # save scaler X
+    outfile = open('../../models/springboot2/new_model/_scalars/scalerX_' + str(i+1) +'.pkl', 'wb')
+    pkl.dump(scalerX, outfile)
+    outfile.close()
+
+    model = models.create_residual_model(trainX.shape[1])
+    opt = Adam(learning_rate=1e-2, decay=1e-3/200)
+    model.compile(loss=root_mean_squared_error, optimizer=opt)
+
+    print('[INFO] training model...')
+    history = model.fit(x=trainX, y=trainY, validation_data=(testX, testY), epochs=200, batch_size=4)
+
+    # save model
+    model.save('../../models/springboot2/new_model/K' + str(i+1))
+
+    loss = history.history['loss'][-1]
+    validation_loss = history.history['val_loss'][-1]
+
+    print('[INFO] predicting residuals...')
+    residual_prediction = model.predict(testX)
+
+    # residual_prediction = residual = xgb_prediction - latency
+    # latency_prediction = xgb_prediction  - (xgb_prediction - latency)
+
+    print('[INFO] calculating latency predictions...')
+    latency_prediction = test['xgb_prediction'] - residual_prediction.flatten()
+
+    _helpers.print_predictions(test['latency'].values, latency_prediction.values)
+
+    print('[RESULT] loss / val_loss', loss, validation_loss)
+    rmse, mae, mape = _helpers.get_error(test['latency'].values, latency_prediction)
+
+    save_predictions_to_csv(i, test, residual_prediction.flatten(), latency_prediction)
+
+    return rmse, mae, mape
+
+def evaluate_model(train_i, test_i, i):
+    
+    df['xgb_prediction'] = xgb.predict(i, df[['concurrent_users', 'heap_size', 'collector', 'size',  'use_case']].values.reshape(-1,5))
+    df['residual'] = df['xgb_prediction'] - df['latency']
+
+    infile = open('../../models/springboot2/new_model/_scalars/scalerX_' + str(i+1) +'.pkl', 'rb')
+    scalerX = pkl.load(infile)
+    infile.close()
+
+    train = df.iloc[train_i]
+    test = df.iloc[test_i]
+
+    model = keras.models.load_model('../../models/springboot2/new_model/K' + str(i+1), compile=False)
+
+    # preds for dataset
+    testX = scalerX.transform(test[['concurrent_users', 'heap_size', 'collector', 'size',  'use_case']].values.reshape(-1,5))
+    testY = test['residual']
+
+    print('[INFO] predicting residuals...')
+    residual_prediction = model.predict(testX)
+
+    # residual_prediction = d_latency  - (d_latency - latency)
+    # latency_prediction = test['xgb_prediction'] - scalerY.inverse_transform(residual_prediction).flatten()
+
+    print('[INFO] calculating latency predictions...')
+    latency_prediction = test['xgb_prediction'] - residual_prediction.flatten()
+
+    # remove negative preds
+    latency_prediction = np.maximum(0.0, latency_prediction)
+
+    # _helpers.print_predictions(test['latency'].values, latency_prediction)
+
+    rmse, mae, mape = _helpers.get_error(test['latency'].values, latency_prediction)
+
+    save_predictions_to_csv(i, test, residual_prediction.flatten(), latency_prediction)
+    
+    return rmse, mae, mape
+
+
+def train_with_cross_validation():
+    errors = {'rmse' : [], 'mae': [], 'mape': []}
+
+    print('[INFO] constructing k fold split...')
+    kf = KFold(n_splits=10, shuffle = True, random_state=14)
+    i = 0
+
+    for train_i, test_i in kf.split(df):
+        print('--------------------------------------------------------------------------------------------------------------------------------')
+        print('K', str(i+1), '\n')
+        rmse, mae, mape = train_model(train_i, test_i, i)
+        errors['rmse'].append(rmse)
+        errors['mae'].append(mae)
+        errors['mape'].append(mape)
+        i += 1
+
+    _helpers.print_errors(errors)
+
+    return errors
+
+def evaluate_with_cross_validation():
+    errors = {'rmse' : [], 'mae': [], 'mape': []}
+    
+    print('[INFO] constructing k fold split...')
+    kf = KFold(n_splits=10, shuffle = True, random_state=14)
+    i = 0
+
+    for train_i, test_i in kf.split(df):
+        print('--------------------------------------------------------------------------------------------------------------------------------')
+        print('\nK', i+1)
+        rmse, mae, mape = evaluate_model(train_i, test_i, i)
+        errors['rmse'].append(rmse)
+        errors['mae'].append(mae)
+        errors['mape'].append(mape)
+
+        i += 1
+
+    _helpers.print_errors(errors)
+    return errors
+
+
+# helper functions
+def save_predictions_to_csv(i, df, residual_prediction, latency_prediction):
+    results_df = pd.DataFrame({'concurrent_users': df['concurrent_users'], 'heap_zize': df['heap_size'], 'collector': df['collector'], 'size': df['size'], 'use_case': df['use_case'], 'latency': df['latency'], 'xgb_prediction': df['xgb_prediction'],'residual': df['residual'] , 'residual_prediction': residual_prediction, 'prediction': latency_prediction})
+    # print(results_df)
+    results_df.to_csv('../../models/springboot2/new_model/results/K' + str(i+1) + '.csv', sep=",", index= False)
+
+# train_with_cross_validation()
+evaluate_with_cross_validation()
